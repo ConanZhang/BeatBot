edfa292f73873bd477cb34bed2c9710ba42f5398
Ryan Dougherty <ryan.dougherty@utah.edu>
2017-10-28 08:06:50 -0600

    added Hz calculation

--- unity-environment/Assets/_Scripts/MoodManager.cs
+++ unity-environment/Assets/_Scripts/MoodManager.cs
@@ -14,7 +14,7 @@ public class MoodManager : Singleton<MoodManager>
 
     private int qSamples = 1024;
     private float refRmsVal = 0.1f; // represents the rms value that corresponds to 0 dB
-    private float freqMinThreshold = 0.02f;
+    private float minSpectrumVal = 0.02f;
 
     private float rmsVal;
     private float dBVal;
@@ -34,6 +34,8 @@ public class MoodManager : Singleton<MoodManager>
     }
     private void AnalyzeSound()
     {
+        // CALCULATING dB
+
         audioSource.GetOutputData(samples, 0);
         float sumSquares = 0.0f;
 
@@ -46,10 +48,40 @@ public class MoodManager : Singleton<MoodManager>
         rmsVal = Mathf.Sqrt(sumSquares / qSamples);
         dBVal = 20 * Mathf.Log10(rmsVal / refRmsVal);
 
+        // calculates a dB val clamped between low and high defined points
         dBVal = Mathf.Clamp(dBVal, min_dB, max_dB);
         Debug.Log("dB val: " + dBVal);
+
+        // CALCULATING Hz
+
+        audioSource.GetSpectrumData(spectrum, 0, FFTWindow.BlackmanHarris);
+        float maxSpectrumVal = 0.0f;
+        int maxSpectrumIndex = 0;
+        for (int i = 0; i < qSamples; i++)
+        {
+            // If you've found a new max value, then assign the max index and spectrum 
+            if(spectrum[i] > maxSpectrumVal && spectrum[i] > minSpectrumVal)
+            {
+                maxSpectrumVal = spectrum[i];
+                maxSpectrumIndex = i;
+            }
+        }
+
+        // interpolating between neighboring values
+        float frequencyAtIndex = maxSpectrumIndex;
+        if(maxSpectrumIndex > 0 && maxSpectrumIndex < qSamples - 1)
+        {
+            float dL = spectrum[maxSpectrumIndex - 1] / spectrum[maxSpectrumIndex];
+            float dR = spectrum[maxSpectrumIndex + 1] / spectrum[maxSpectrumIndex];
+            frequencyAtIndex += 0.5f * (dR * dR - dL * dL);
+        }
+
+        // calculate the dominant frequency at that frame
+        pitchVal = frequencyAtIndex * (sampleRate / 2) / qSamples;
+        Debug.Log("Hz val: " + pitchVal);
     }
 
+    
     private void Update()
     {
         AnalyzeSound();
