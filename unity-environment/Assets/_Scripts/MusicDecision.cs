c733805c02661c0ad0eadf4591b4e280a64e7d75
Ryan Dougherty <ryan.dougherty@utah.edu>
2017-10-27 22:23:45 -0600

    added basic template scripts

---
+++ unity-environment/Assets/_Scripts/MusicDecision.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicDecision : MonoBehaviour, Decision
+{
+
+    public float[] Decide(List<float> state, List<Camera> observation, float reward, bool done, float[] memory)
+    {
+        return new float[4]{ 0f, 0f, 0f, 0.0f };
+    }
+
+    public float[] MakeMemory(List<float> state, List<Camera> observation, float reward, bool done, float[] memory)
+    {
+        return new float[0];
+    }
+}
