898db12b3338099483b6f26d6ad1c286de425e4d
Ryan Dougherty <ryan.dougherty@utah.edu>
2017-10-28 05:40:22 -0600

    added music visualizer effect

--- unity-environment/Assets/_Scripts/MoodManager.cs
+++ unity-environment/Assets/_Scripts/MoodManager.cs
@@ -4,6 +4,56 @@ using UnityEngine;
 
 public class MoodManager : Singleton<MoodManager> 
 {
+    AudioSource audioSource;
+    [Header("Clamping dB values")]
+    public float min_dB = -25.0f;
+    public float max_dB = 25.0f;
+
     [Header("The all seeing, all knowing mood value that we must edit")]
     public float MoodValue = 0.5f;
+
+    private int qSamples = 1024;
+    private float refRmsVal = 0.1f; // represents the rms value that corresponds to 0 dB
+    private float freqMinThreshold = 0.02f;
+
+    private float rmsVal;
+    private float dBVal;
+    private float pitchVal;
+
+    private float[] samples;
+    private float[] spectrum;
+    private float sampleRate;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        samples = new float[qSamples];
+        spectrum = new float[qSamples];
+        sampleRate = AudioSettings.outputSampleRate;
+    }
+    private void AnalyzeSound()
+    {
+        audioSource.GetOutputData(samples, 0);
+        float sumSquares = 0.0f;
+
+        // Sum up the square of all of the samples
+        for (int i = 0; i < qSamples; i++)
+        {
+            sumSquares += Mathf.Pow(samples[i], 2);
+        }
+
+        rmsVal = Mathf.Sqrt(sumSquares / qSamples);
+        dBVal = 20 * Mathf.Log10(rmsVal / refRmsVal);
+
+        dBVal = Mathf.Clamp(dBVal, min_dB, max_dB);
+        Debug.Log("dB val: " + dBVal);
+    }
+
+    private void Update()
+    {
+        AnalyzeSound();
+        MoodValue = (dBVal + Mathf.Abs(min_dB)) / (max_dB + Mathf.Abs(min_dB));
+    }
+
 }
