814633a5e035cd5e47026ebf18f57d8e4df4bcf7
Ryan Dougherty <ryan.dougherty@utah.edu>
2017-10-28 08:48:43 -0600

    Added sampling support

--- unity-environment/Assets/_Scripts/MoodManager.cs
+++ unity-environment/Assets/_Scripts/MoodManager.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
 using UnityEngine;
 
 public class MoodManager : Singleton<MoodManager> 
@@ -12,11 +15,16 @@ public class MoodManager : Singleton<MoodManager>
     [Header("The all seeing, all knowing mood value that we must edit")]
     public float MoodValue = 0.5f;
 
+    [Header("File paths for output data files")]
+    public string lowValueFilePath = "Assets/_Data/lowVals.txt";
+    public string highValueFilePath = "Assets/_Data/highVals.txt";
+
     private int qSamples = 1024;
     private float refRmsVal = 0.1f; // represents the rms value that corresponds to 0 dB
     private float minSpectrumVal = 0.02f;
 
     private float rmsVal;
+
     private float dBVal;
     private float pitchVal;
 
@@ -24,9 +32,16 @@ public class MoodManager : Singleton<MoodManager>
     private float[] spectrum;
     private float sampleRate;
 
+    // Lists of high and low values stored in format dB, Hz 
+    private List<Tuple<float, float>> highValues;
+    private List<Tuple<float, float>> lowValues;
+
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
+
+        highValues = new List<Tuple<float, float>>();
+        lowValues = new List<Tuple<float, float>>();
 
         samples = new float[qSamples];
         spectrum = new float[qSamples];
@@ -81,11 +96,81 @@ public class MoodManager : Singleton<MoodManager>
         Debug.Log("Hz val: " + pitchVal);
     }
 
-    
+    private void CaptureHighSound()
+    {
+        highValues.Add(new Tuple<float, float>(dBVal, pitchVal));
+    }
+    private void CaptureLowSound()
+    {
+        lowValues.Add(new Tuple<float, float>(dBVal, pitchVal));
+    }
+
     private void Update()
     {
         AnalyzeSound();
         MoodValue = (dBVal + Mathf.Abs(min_dB)) / (max_dB + Mathf.Abs(min_dB));
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            CaptureHighSound();
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            CaptureLowSound();
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        Debug.Log("Low size: " + lowValues.Count);
+        Debug.Log("High size: " + highValues.Count);
+
+        StreamWriter lowValueWriter = new StreamWriter(lowValueFilePath, true);
+        StreamWriter highValueWriter = new StreamWriter(highValueFilePath, true);
+
+        float avgLowFreq, avgHighFreq;
+        float avgLowdB, avgHighdB;
+        float freqSum = 0.0f, dBSum = 0.0f;
+
+        // Calculate the average for low values
+        foreach (Tuple<float, float> pair in lowValues)
+        {
+            dBSum += pair.Item1;
+            freqSum += pair.Item2;
+        }
+        avgLowdB = dBSum / lowValues.Count;
+        avgLowFreq = freqSum / lowValues.Count;
+
+        Debug.Log("avg low: " + avgLowdB + " " + avgLowFreq);
+
+        // Writing values into files
+        lowValueWriter.WriteLine(avgLowdB);
+        lowValueWriter.WriteLine(avgLowFreq);
+
+        lowValueWriter.Close();
+
+        AssetDatabase.ImportAsset(lowValueFilePath);
+
+        dBSum = 0.0f;
+        freqSum = 0.0f;
+
+        // Calculate the average for high values
+        foreach (Tuple<float, float> pair in highValues)
+        {
+            dBSum += pair.Item1;
+            freqSum += pair.Item2;
+        }
+        avgHighdB = dBSum / highValues.Count;
+        avgHighFreq = freqSum / highValues.Count;
+
+        Debug.Log("avg high: " + avgHighdB + " " + avgHighFreq);
+
+        highValueWriter.WriteLine(avgHighdB);
+        highValueWriter.WriteLine(avgHighFreq);
+
+        highValueWriter.Close();
+
+        AssetDatabase.ImportAsset(highValueFilePath);
     }
 
 }
