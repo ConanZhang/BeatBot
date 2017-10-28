192efd83e2ca9b815fa16ad46c59cc6dc1831485
ConanZhang <conankzhang@gmail.com>
2017-10-28 01:18:43 -0600

    Created test environment

---
+++ unity-environment/Assets/_Scripts/Conan/TestAgent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAgent : Agent{
+
+    public override List<float> CollectState()
+    {
+        List<float> state = new List<float>();
+        return state;
+    }
+
+    // to be implemented by the developer
+    public override void AgentStep(float[] act)
+    {
+    }
+
+    // to be implemented by the developer
+    public override void AgentReset()
+    {
+    }
+}
